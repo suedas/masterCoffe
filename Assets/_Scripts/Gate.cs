98ef9b70e6b41c0718f536842c93baaec53d67e6
suedas <55928205+suedas@users.noreply.github.com>
2022-03-07 23:50:32 +0300

    Negative Gate

--- Assets/_Scripts/Gate.cs
+++ Assets/_Scripts/Gate.cs
@@ -4,26 +4,136 @@ using UnityEngine;
 
 public class Gate : MonoBehaviour
 {
+
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
-
-        if (other.gameObject.CompareTag("-2") )
+        #region NegativeGate
+        if (other.gameObject.CompareTag("-2"))
         {//listeden de silencek mi
-       
+
             if (GameObject.FindWithTag("sol"))
-            {
-                Debug.Log("ssssssss");
-                int child=gameObject.transform.childCount-2;
-                Debug.Log(child + "   child");
+            {//kontrolü unutma child ý var mý diye 
+                if (SwerveMovement.instance.leftParent.childCount > 2)
+                {
+                    for (int i = 0; i < 2; i++)
+                    {
+                        Destroy(SwerveMovement.instance.leftParent.transform.GetChild(SwerveMovement.instance.leftParent.transform.childCount - 1).gameObject);
+
+
+                    }
+                    Debug.Log("ssssssss");
+                    //Transform child = gameObject.transform.GetChild(transform.childCount - 3);
+                    //Destroy(child.gameObject);
+
+                    Debug.Log("sol" + SwerveMovement.instance.leftParent.childCount);
+
+
+                }
+                else
+                {
+                    //losePanel
+                }
+
 
             }
             else if (GameObject.FindWithTag("sag"))
             {
+                if (SwerveMovement.instance.rightParent.childCount > 2)
+                {
+                    for (int i = 0; i < 2; i++)
+                    {
+                        Destroy(SwerveMovement.instance.rightParent.transform.GetChild(SwerveMovement.instance.rightParent.transform.childCount - 1).gameObject);
 
+
+                    }
+                }
+                else
+                {
+                    //losePanel
+                }
             }
         }
 
-       
+        else if (other.gameObject.CompareTag("-10"))
+        {
+            if (GameObject.FindWithTag("sol"))
+            {//kontrolü unutma child ý var mý diye 
+                if (SwerveMovement.instance.leftParent.childCount > 10)
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        Destroy(SwerveMovement.instance.leftParent.transform.GetChild(SwerveMovement.instance.leftParent.transform.childCount - 1).gameObject);
+
+
+                    }
+
+                }
+                else
+                {
+                    //losePanel
+                }
+
+            }
+            else if (GameObject.FindWithTag("sag"))
+            {
+                if (SwerveMovement.instance.rightParent.childCount > 10)
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        Destroy(SwerveMovement.instance.rightParent.transform.GetChild(SwerveMovement.instance.rightParent.transform.childCount - 1).gameObject);
+
+
+                    }
+                }
+                else
+                {
+                    //losePanel
+                }
+            }
+        }
+        else if (other.gameObject.CompareTag("bölü2"))
+        {
+            if (GameObject.FindWithTag("sol"))
+            {//kontrolü unutma child ý var mý diye 
+                if (SwerveMovement.instance.leftParent.childCount > 1)
+                {
+                    for (int i = 0; i < SwerveMovement.instance.leftParent.childCount / 2; i++)
+                    {
+                        Destroy(SwerveMovement.instance.leftParent.transform.GetChild(SwerveMovement.instance.leftParent.transform.childCount - 1).gameObject);
+
+
+                    }
+
+                }
+                else
+                {
+                    //losePanel
+                }
+
+            }
+            else if (GameObject.FindWithTag("sag"))
+            {
+                if (SwerveMovement.instance.rightParent.childCount > 1)
+                {
+                    for (int i = 0; i < SwerveMovement.instance.leftParent.childCount / 2; i++)
+                    {
+                        Destroy(SwerveMovement.instance.rightParent.transform.GetChild(SwerveMovement.instance.rightParent.transform.childCount - 1).gameObject);
+
+
+                    }
+                }
+                else
+                {
+                    //losePanel
+                }
+
+
+
+            }
+        }
+        #endregion
+        #region PozitiveGate
+        #endregion
     }
 }
